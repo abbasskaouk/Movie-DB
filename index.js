6c494193373a15b0c620bee637c2bdec93f68285
abbasskaouk <abbassikaouk@gmail.com>
2021-06-14 12:26:42 +0300

    step 5

--- index.js
+++ index.js
@@ -3,7 +3,12 @@ const app = express()
 const port = 3000
 var d = new Date();
 var date = d.getHours() + ":" + d.getMinutes();
-
+const movies = [
+    { title: 'Jaws', year: 1975, rating: 8 },
+    { title: 'Avatar', year: 2009, rating: 7.8 },
+    { title: 'Brazil', year: 1985, rating: 8 },
+    { title: 'الإرهاب والكباب‎', year: 1992, rating: 6.2 }
+];
 /*******************************
  * Step 2 
  * 
@@ -60,10 +65,28 @@ app.get('/search', (req, res) => {
 
 
 
+/**********
+ * 
+ * Step 5 
+ * create: /movies/add, /movies/get, /movies/edit, /movies/delete,
+ * make /movies/get returns the movies in movies array
+ * 
+ */
+app.get('/movies/add', (req, res) => {
 
+  })
 
+app.get('/movies/get', (req, res) => {
+    res.send({status:200, data:movies});
+})
 
+app.get('/movies/edit', (req, res) => {
 
+})
+
+app.get('/movies/delete', (req, res) => {
+
+})
 
 
 
